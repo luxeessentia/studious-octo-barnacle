465bcf8df5c22cb7df155001afc20006e8ff321e
luxeessentia <essentialsluxe26@gmail.com>
2025-09-04 18:30:49 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,12 @@
+import PromoBar from './PromoBar';
+import Header from './Header';
+import Footer from './Footer';
+import '../styles/globals.css';
+
 export default function Layout({ children }) {
   return (
     <>
       <PromoBar />
       <Header />
-      <main className='container' style={{ paddingTop: 16, paddingBottom: 160 }}>
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
-}
+      <main className="container" style={{ paddingTop: 16, paddingBottom: 160 }}>
+        {children
